6e10b91013cbbcb08799e547bd31db6a8abec210
Andy McKay <amckay@mozilla.com>
2012-01-19 17:11:24 -0800

    first commit

---
+++ mysql_pool/base.py
@@ -0,0 +1,50 @@
+from sqlalchemy.pool import QueuePool
+
+from django.db.backends.mysql.base import *
+from functools import partial
+
+
+def null(*args, **kwargs):
+    pass
+
+mypool = QueuePool(null, **settings.DATABASE_POOL_ARGS)
+
+# DATABASE_POOL_ARGS should be something like:
+# {'max_overflow':10, 'pool_size':5, 'recycle':300}
+
+class DatabaseWrapper(DatabaseWrapper):
+
+    def _cursor(self):
+        if not self._valid_connection():
+            kwargs = {
+                'conv': django_conversions,
+                'charset': 'utf8',
+                'use_unicode': True,
+            }
+            settings_dict = self.settings_dict
+            if settings_dict['USER']:
+                kwargs['user'] = settings_dict['USER']
+            if settings_dict['NAME']:
+                kwargs['db'] = settings_dict['NAME']
+            if settings_dict['PASSWORD']:
+                kwargs['passwd'] = settings_dict['PASSWORD']
+            if settings_dict['HOST'].startswith('/'):
+                kwargs['unix_socket'] = settings_dict['HOST']
+            elif settings_dict['HOST']:
+                kwargs['host'] = settings_dict['HOST']
+            if settings_dict['PORT']:
+                kwargs['port'] = int(settings_dict['PORT'])
+            # We need the number of potentially affected rows after an
+            # "UPDATE", not the number of changed rows.
+            kwargs['client_flag'] = CLIENT.FOUND_ROWS
+            kwargs.update(settings_dict['OPTIONS'])
+
+            mypool._creator = partial(Database.connect, **kwargs)
+            self.connection = mypool.connect()
+
+            self.connection.encoders[SafeUnicode] = self.connection.encoders[unicode]
+            self.connection.encoders[SafeString] = self.connection.encoders[str]
+            connection_created.send(sender=self.__class__, connection=self)
+
+        cursor = CursorWrapper(self.connection.cursor())
+        return cursor
