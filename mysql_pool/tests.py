819989d01c2a80e4a61439c866384586da46077f
Andy McKay <amckay@mozilla.com>
2012-01-23 11:37:17 -0800

    add in event test

--- mysql_pool/tests.py
+++ mysql_pool/tests.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.cache import cache
 from django.utils import unittest
 
 from mock import Mock
 from mysql_pool.base import DatabaseWrapper, db_pool
 
+from sqlalchemy.pool import QueuePool
 from sqlalchemy import event
 
 # Alright I'm tired and don't have the time to mock this out fully. I just
@@ -54,8 +56,17 @@ class TestPool(unittest.TestCase):
 
     def setUp(self):
         # Make sure we start with nothing in the pool.
+        cache.clear()
         db_pool.close()
         self.wrapper = DatabaseWrapper(Mock())
+        self.events = []
+        self.connect = lambda *args: self.events.append('connect')
+        self.checkin = lambda *args: self.events.append('checkin')
+        self.checkout = lambda *args: self.events.append('checkout')
+        event.listen(QueuePool, 'connect', self.connect)
+        event.listen(QueuePool, 'checkin', self.checkin)
+        event.listen(QueuePool, 'checkout', self.checkout)
+        self.qs = User.objects.all()
 
     def test_serialize(self):
         _keys = []
@@ -65,17 +76,20 @@ class TestPool(unittest.TestCase):
         assert len(set(_keys)) == len(databases)
 
     def test_can_query(self):
-        len(User.objects.all())
+        len(self.qs)
         assert len(db_pool.pools) == 1
 
     def test_multiple_queries(self):
         for x in range(3):
-            len(User.objects.all())
+            len(self.qs)
         assert len(db_pool.pools) == 1
 
     def test_multiple_pool(self):
         for db in databases:
-            User.objects.using(db).all()
-        assert len(db_pool.pools) == 2
+            len(User.objects.using(db).all())
+        assert len(db_pool.pools) == len(databases)
 
-
+    def test_connect(self):
+        len(self.qs)
+        assert len(db_pool.pools) == 1
+        assert set(['checkin', 'checkout', 'connect']) == set(self.events)
